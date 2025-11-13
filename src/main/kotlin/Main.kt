8a936b7e30658af54cb4a83c8b4da1b50b708ff5
daniil <daniil.rakhmatulin@gmail.com>
2025-11-13 12:59:29 +0500

    adds corr matrix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -34,8 +34,8 @@ fun main() {
 
     val codes = mutableListOf<Pair<Double, IntArray>>()
     var a = 0.0
-    while (a < 100.0) {
-        a += 1.0
+    while (a < 360.0) {
+        a += 36.0
 
         var x = -1.0
         while (x <= 1.0) {
@@ -56,33 +56,35 @@ fun main() {
 
 
 
-    val matrix = showAngleCodesCorrelationHeatmap(codes)
-    showSimilarityCurve(matrix, 0.0)
+//    val matrix = showAngleCodesCorrelationHeatmap(codes)
+//    showSimilarityCurve(matrix, 0.0)
 
     val emptyCodes: List<Pair<Double?, IntArray>> = (0..300).map { null to IntArray(encoder.codeSizeInBits) }
 
     // CPU processing
-//    val cpuTime = measureTime {
-//        val layout = DampLayout2D(angleCodes = codes + emptyCodes)
-//        val outCPU = layout.layoutLongRange(
-//            farRadius = 20,
-//            epochs = 100,
-//            minSim = 0.00,
-//            lambdaStart = 0.30,
-//            lambdaEnd = 0.90,
-//            eta = 0.0,
-//            maxBatchFrac = 0.30,
-//            log = false
-//        )
-//        showAnglesGrid(outCPU.map { it.first })
-//    }
-//    println("CPU Layout finished! Total time: $cpuTime")
+    val cpuTime = measureTime {
+        val c = (codes + emptyCodes).shuffled()
+        val layout = DampLayout2D(angleCodes = codes + emptyCodes)
+        showAnglesGrid(c.map { it.first })
+
+        val outCPU = layout.layoutLongRange(
+            farRadius = 20,
+            epochs = 100,
+            minSim = 0.00,
+            lambdaStart = 0.30,
+            lambdaEnd = 0.90,
+            eta = 0.0,
+            maxBatchFrac = 0.30,
+            log = false
+        )
+        showAnglesGrid(outCPU.map { it.first })
+    }
+    println("CPU Layout finished! Total time: $cpuTime")
 
     // GPU processing
 //    val c = (codes + emptyCodes).shuffled()
 //    val gpuTime = measureTime {
 //        showAnglesGrid(c.map { it.first })
-//        showAnglesGridIso(c.map { it.first })
 //
 //        val gpuLayout = GpuDamlLayout2D_GL430(c)
 //        val outGPU =  gpuLayout.layoutLongRange(
@@ -96,7 +98,6 @@ fun main() {
 //        )
 //        gpuLayout.dispose()
 //        showAnglesGrid(outGPU.map { it.first })
-//        showAnglesGridIso(outGPU.map { it.first })
 //    }
 //    println("GPU Layout finished! Total time: $gpuTime")
 
